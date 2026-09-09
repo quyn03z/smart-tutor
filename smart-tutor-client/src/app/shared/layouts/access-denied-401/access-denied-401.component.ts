eccf412d5f6b6e8ec638b1cb5e016c7d47895d6a
quyn03z <quyenvn03@gmail.com>
2026-09-09 10:53:02 +0700

    add new base frontend,backend smart tutor

---
+++ smart-tutor-client/src/app/shared/layouts/access-denied-401/access-denied-401.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access-denied-401',
+  standalone: true,
+  imports: [],
+  templateUrl: './access-denied-401.component.html',
+  styleUrl: './access-denied-401.component.scss'
+})
+export class AccessDenied401Component {
+
+}
