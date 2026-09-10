6716d92539c4d8fb48a54497561db70eb3592416
quyn03z <quyenvn03@gmail.com>
2026-09-10 14:23:54 +0700

    add claimService

---
+++ smart-tutor-server/SmartTotur.DataAccess/Claims/ClaimService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace SmartTutor.DataAccess.Claims
+{
+    public class ClaimService : IClaimService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ClaimService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int? GetUserId()
+        {
+            var idString = GetClaim(ClaimTypes.NameIdentifier);
+            if (int.TryParse(idString, out var userId))
+            {
+                return userId;
+            }
+            return null;
+        }
+
+        public string? GetClaim(string key)
+        {
+            return _httpContextAccessor.HttpContext?.User?.FindFirst(key)?.Value;
+        }
+
+        public string? GetIpAddress()
+        {
+            var ip = _httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString();
+            if (_httpContextAccessor.HttpContext?.Request.Headers.ContainsKey("X-Forwarded-For") == true)
+            {
+                ip = _httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"].ToString();
+            }
+            return ip;
+        }
+
+        public string? GetUserAgent()
+        {
+            return _httpContextAccessor.HttpContext?.Request.Headers["User-Agent"].ToString();
+        }
+
+
+    }
+}
---
+++ smart-tutor-server/SmartTotur.DataAccess/Claims/IClaimService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartTutor.DataAccess.Claims
+{
+    public interface IClaimService
+    {
+        int? GetUserId();
+        string? GetClaim(string key);
+        string? GetIpAddress();
+        string? GetUserAgent();
+    }
+}
