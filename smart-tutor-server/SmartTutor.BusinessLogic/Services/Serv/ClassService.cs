dc57fd239a7806f3cf2ac97b768f8af4094e827d
quyn03z <quyenvn03@gmail.com>
2026-09-14 11:56:11 +0700

    add class controller

--- smart-tutor-server/SmartTutor.BusinessLogic/Services/Serv/ClassService.cs
+++ smart-tutor-server/SmartTutor.BusinessLogic/Services/Serv/ClassService.cs
@@ -1,11 +1,29 @@
-﻿using SmartTutor.BusinessLogic.Services.Impl;
+﻿using SmartTutor.BusinessLogic.Models;
+using SmartTutor.BusinessLogic.Services.Impl;
+using SmartTutor.DataAccess.Claims;
+using SmartTutor.DataAccess.Repositories.Impl;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static SmartTutor.BusinessLogic.Models.ClassModels;
 
 namespace SmartTutor.BusinessLogic.Services.Serv
 {
     public class ClassService : IClassService
     {
+        private readonly IClaimService _claimService;
+        private readonly IClassRepository _classRepository;
+
+        public ClassService(IClaimService claimService, IClassRepository classRepository)
+        {
+            _claimService = claimService;
+            _classRepository = classRepository;
+        }
+
+        public async Task<ClassResponseModel> AddClassAsync(ClassRequestModel classRequestModel)
+        {
+            var userId = _claimService.GetUserId();
+        }
+
     }
 }
