22852fc52cbdae26a742e059e86680ec39f99adc
quyn03z <quyenvn03@gmail.com>
2026-09-14 10:58:03 +0700

    add class models

---
+++ smart-tutor-server/SmartTutor.BusinessLogic/Models/ClassModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SmartTutor.BusinessLogic.Models
+{
+    public class ClassModels
+    {
+
+        public class ClassResponseModel
+        {
+            public int Id { get; set; }
+            public int UserId { get; set; }
+            public string ClassName { get; set; } = string.Empty;
+            public string ClassType { get; set; } = string.Empty;
+            public decimal DefaultFeePerSession { get; set; }
+            public string? SchedulePattern { get; set; }
+        }
+
+        public class ClassRequestModel 
+        {
+            public int Id { get; set; }
+            public int UserId { get; set; }
+
+            [Required(ErrorMessage = "Tên lớp là bắt buộc.")]
+            [StringLength(100, ErrorMessage = "Tên lớp không được vượt quá 100 ký tự.")]
+            public string ClassName { get; set; } = string.Empty;
+            public string ClassType { get; set; } = string.Empty;
+            public decimal DefaultFeePerSession { get; set; }
+            public string? SchedulePattern { get; set; }
+
+        }
+
+    }
+}
--- smart-tutor-server/SmartTutor.BusinessLogic/Services/Impl/IClassService.cs
+++ smart-tutor-server/SmartTutor.BusinessLogic/Services/Impl/IClassService.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static SmartTutor.BusinessLogic.Models.ClassModels;
 
 namespace SmartTutor.BusinessLogic.Services.Impl
 {
     public interface IClassService
     {
+        Task<ClassResponseModel> AddClassAsync(ClassRequestModel classRequestModel);
+
     }
 }
