59f83ef364469299bf468bfc2d3db56b8def1d6a
quyn03z <quyenvn03@gmail.com>
2026-09-14 14:04:55 +0700

    add api create class

--- smart-tutor-server/SmartTutor.API/Controllers/ClassController.cs
+++ smart-tutor-server/SmartTutor.API/Controllers/ClassController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartTutor.BusinessLogic.Models;
+using SmartTutor.BusinessLogic.Services.Impl;
+using SmartTutor.BusinessLogic.Services.Serv;
+using static SmartTutor.BusinessLogic.Models.ClassModels;
+using static SmartTutor.BusinessLogic.Models.StudentModels;
 
 namespace SmartTutor.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ClassController : ControllerBase
+    public class ClassController : BaseController
     {
+        private readonly IClassService _classService;
+
+        public ClassController(IClassService classService)
+        {
+            _classService = classService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateClassAsync(ClassRequestModel classRequestModel)
+        {
+            if (!ModelState.IsValid)
+                return ValidationError();
+            return Ok(ApiResult<ClassResponseModel>
+                .Success(await _classService.CreateClassAsync(classRequestModel)));
+        }
+
     }
 }
--- smart-tutor-server/SmartTutor.BusinessLogic/Services/Serv/ClassService.cs
+++ smart-tutor-server/SmartTutor.BusinessLogic/Services/Serv/ClassService.cs
@@ -2,6 +2,9 @@
 using SmartTutor.BusinessLogic.Services.Impl;
 using SmartTutor.DataAccess.Claims;
 using SmartTutor.DataAccess.Repositories.Impl;
+using SmartTutor.DataAccess.Repositories.Repo;
+using SmartTutor.Domain.Enums;
+using SmartTutor.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,9 +23,33 @@ namespace SmartTutor.BusinessLogic.Services.Serv
             _classRepository = classRepository;
         }
 
-        public async Task<ClassResponseModel> AddClassAsync(ClassRequestModel classRequestModel)
+        public async Task<ClassResponseModel> CreateClassAsync(ClassRequestModel classRequestModel)
         {
             var userId = _claimService.GetUserId();
+            if (!userId.HasValue)
+                throw new UnauthorizedAccessException("Người dùng chưa đăng nhập hoặc token không hợp lệ.");
+
+            var @class = new Class
+            {
+                UserId = userId.Value,
+                ClassName = classRequestModel.ClassName,
+                ClassType = classRequestModel.ClassType,
+                DefaultFeePerSession = classRequestModel.DefaultFeePerSession,
+                SchedulePattern = classRequestModel.SchedulePattern,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            var newClass = await _classRepository.AddAsync(@class);
+
+            return new ClassResponseModel
+            {
+                Id = newClass.Id,
+                UserId = newClass.UserId,
+                ClassName = newClass.ClassName,
+                ClassType = newClass.ClassType,
+                DefaultFeePerSession = newClass.DefaultFeePerSession,
+                SchedulePattern = newClass.SchedulePattern
+            };
         }
 
     }
