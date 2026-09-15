c967da5acb891e7a9b54a645d53601de76393402
quyn03z <quyenvn03@gmail.com>
2026-09-15 14:21:02 +0700

    add repo

---
+++ smart-tutor-server/SmartTotur.DataAccess/Repositories/Repo/SessionRepository.cs
@@ -0,0 +1,16 @@
+﻿using SmartTutor.DataAccess.Persistence;
+using SmartTutor.DataAccess.Repositories.Impl;
+using SmartTutor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartTutor.DataAccess.Repositories.Repo
+{
+    public class SessionRepository : BaseRepository<Session>, ISessionRepository
+    {
+        public SessionRepository(SmartTutorContext context) : base(context)
+        {
+        }
+    }
+}
