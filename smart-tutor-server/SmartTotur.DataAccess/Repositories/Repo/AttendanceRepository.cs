96748d9817661c4cc72afe4d20ddd9e5a99cb441
quyn03z <quyenvn03@gmail.com>
2026-09-19 09:41:39 +0700

    add  repo

---
+++ smart-tutor-server/SmartTotur.DataAccess/Repositories/Repo/AttendanceRepository.cs
@@ -0,0 +1,16 @@
+using SmartTutor.DataAccess.Persistence;
+using SmartTutor.DataAccess.Repositories.Impl;
+using SmartTutor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartTutor.DataAccess.Repositories.Repo
+{
+    public class AttendanceRepository : BaseRepository<AttendanceLog>, IAttendanceRepository
+    {
+        public AttendanceRepository(SmartTutorContext context) : base(context)
+        {
+        }
+    }
+}
