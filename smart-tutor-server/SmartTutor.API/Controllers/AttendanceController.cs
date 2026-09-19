c108f6af4d53ae683ed2574f7809223093038f18
quyn03z <quyenvn03@gmail.com>
2026-09-19 09:41:47 +0700

    add controller

--- smart-tutor-server/SmartTutor.API/Controllers/AttendanceController.cs
+++ smart-tutor-server/SmartTutor.API/Controllers/AttendanceController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartTutor.BusinessLogic.Models;
+using SmartTutor.BusinessLogic.Services.Impl;
+using static SmartTutor.BusinessLogic.Models.AttendanceModels;
 
 namespace SmartTutor.API.Controllers
 {
+    [Authorize]
     [Route("api/attendance")]
     [ApiController]
-    public class AttendanceController : ControllerBase
+    public class AttendanceController : BaseController
     {
+        private readonly IAttendanceService _attendanceService;
+
+        public AttendanceController(IAttendanceService attendanceService)
+        {
+            _attendanceService = attendanceService;
+        }
+
+        [HttpPut]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAttendanceRecordAsync([FromBody] UpdateAttendanceRequestDto updateAttendanceRequestDto, int? id)
+        {
+            if (id.HasValue && id.Value > 0 && updateAttendanceRequestDto.AttendanceId <= 0)
+            {
+                updateAttendanceRequestDto.AttendanceId = id.Value;
+            }
+
+            if (!ModelState.IsValid)
+                return ValidationError();
+
+            return Ok(ApiResult<UpdateAttendanceResponseDto>
+                .Success(await _attendanceService.UpdateAttendanceAsync(updateAttendanceRequestDto)));
+        }
+
     }
 }
--- smart-tutor-server/SmartTutor.API/Controllers/ReportsController.cs
+++ smart-tutor-server/SmartTutor.API/Controllers/ReportsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartTutor.BusinessLogic.Models;
+using SmartTutor.BusinessLogic.Services.Impl;
+using SmartTutor.BusinessLogic.Services.Serv;
+using static SmartTutor.BusinessLogic.Models.UserModels;
 
 namespace SmartTutor.API.Controllers
 {
@@ -7,5 +11,21 @@ namespace SmartTutor.API.Controllers
     [ApiController]
     public class ReportsController : BaseController
     {
+        private readonly IRepoortService _repoortService;
+
+        public ReportsController(IRepoortService repoortService)
+        {
+            _repoortService = repoortService;
+        }
+
+        [HttpPost("generate")]
+        public async Task<IActionResult> GenerateMonthlyReports([FromBody] GenerateReportRequestDto dto)
+        {
+            if (!ModelState.IsValid)
+                return ValidationError();
+            return Ok(ApiResult<CreateUserResponseModel>
+                .Success(await _repoortService.CreateUserAsync(createUserModel)));
+        }
+
     }
 }
