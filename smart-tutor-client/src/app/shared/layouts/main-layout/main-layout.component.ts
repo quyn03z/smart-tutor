52e0aad46be2a4fe8da621b91a0545bec1a8ae8b
quyn03z <quyenvn03@gmail.com>
2026-09-21 16:53:39 +0700

    add layouts

--- smart-tutor-client/src/app/shared/layouts/main-layout/main-layout.component.ts
+++ smart-tutor-client/src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
