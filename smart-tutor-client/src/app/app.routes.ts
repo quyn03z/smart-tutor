e3f3eddc28d8d52e1283c41ff2386eab9664455b
quyn03z <quyenvn03@gmail.com>
2026-09-22 14:25:11 +0700

    Update app.routes.ts

--- smart-tutor-client/src/app/app.routes.ts
+++ smart-tutor-client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/account/login/login.component';
 import { RegisterComponent } from './modules/account/register/register.component';
+import { ForgotPasswordComponent } from './modules/account/forgot-password/forgot-password.component';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -15,9 +16,10 @@ export const routes: Routes = [
     ]
   },
 
-  // 2. Trang Xác thực (Login / Register)
+  // 2. Trang Xác thực (Login / Register / Forgot Password)
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
 
   // 3. Fallback route
   { path: '**', redirectTo: '' }
