50e39c169118033098f4aee268b19155908ed58d
quyn03z <quyenvn03@gmail.com>
2026-09-22 10:00:19 +0700

    Update header.component.ts

--- smart-tutor-client/src/app/shared/layouts/header/header.component.ts
+++ smart-tutor-client/src/app/shared/layouts/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isScrolled = false;
+  isMobileMenuOpen = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 20;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
